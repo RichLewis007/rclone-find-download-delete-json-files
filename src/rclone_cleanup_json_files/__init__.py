788627536aff08cf4bfc715f6a6d606aa28f38da
Rich Lewis <1149213+RichLewis007@users.noreply.github.com>
2026-02-27 09:46:41 -0500

    dirs

---
+++ src/rclone_cleanup_json_files/__init__.py
@@ -0,0 +1,6 @@
+"""Find, download and delete JSON files from cloud drives.
+
+Author: Rich Lewis - GitHub: @RichLewis007
+"""
+
+__version__ = "0.1.0"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,1 @@
-"""Tests for rclone_json_tui."""
+"""Tests for rclone_cleanup_json_files."""
